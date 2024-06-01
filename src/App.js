4ea07d939a426f895e8b9c5031d66b8bc4df6195
vaibhav sharma <vaibhav@softment.com>
2024-06-01 14:46:02 +0530

    style fixes

--- src/App.js
+++ src/App.js
@@ -1,75 +1,129 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./Components/Home";
 import Loader from "./Components/Loader";
 import Login from "./Components/Login";
 import { auth } from "./config/firebase-config";
 import CommonRoute from "./routes/CommonRoute";
 import colors from "./theme/colors";
-import './styles/style.css'
+import "./styles/style.css";
 import UserDetail from "./Components/UserDetail";
 import Coupon from "./Components/Coupon";
 import Report from "./Components/Report";
 import Notification from "./Components/Notification";
 import ReportParent from "./Components/ReportParent";
 const App = () => {
-  const [isLoading,setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const theme = createTheme({
     typography: {
       button: {
-        textTransform: "none"
+        textTransform: "none",
       },
-      fontFamily: [
-        'Times New Roman',
-      ].join(','),
+      fontFamily: ["Times New Roman"].join(","),
     },
     components: {
       MuiButton: {
         styleOverrides: {
           root: ({ ownerState }) => ({
-            '&:hover': {
-              backgroundColor:ownerState.variant=="contained"&&colors.appPrimary
+            "&:hover": {
+              backgroundColor:
+                ownerState.variant == "contained" && colors.appPrimary,
             },
           }),
         },
       },
     },
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
-        setIsLoading(false)
-        navigate("/")
+        setIsLoading(false);
+        navigate("/");
       } else {
-        setIsLoading(false)
-        navigate("/login")
+        setIsLoading(false);
+        navigate("/login");
       }
     });
-    return unsubscribe
-  }, [])
-  if(isLoading){
-    return(
-      <Loader/>
-    )
+    return unsubscribe;
+  }, []);
+  if (isLoading) {
+    return <Loader />;
   }
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route exact element={<CommonRoute><Login /></CommonRoute>} path='/login' />
-        <Route exact element={<CommonRoute authRequire><Home /></CommonRoute>} path='/' />
+        <Route
+          exact
+          element={
+            <CommonRoute>
+              <Login />
+            </CommonRoute>
+          }
+          path="/login"
+        />
+        <Route
+          exact
+          element={
+            <CommonRoute authRequire>
+              <Home />
+            </CommonRoute>
+          }
+          path="/"
+        />
         {/* <Route exact element={<CommonRoute authRequire><Home /></CommonRoute>} path='/user' /> */}
-        <Route exact element={<CommonRoute authRequire><Notification /></CommonRoute>} path='/notifications' />
-        <Route exact element={<CommonRoute authRequire><ReportParent /></CommonRoute>} path='/reports' />
+        <Route
+          exact
+          element={
+            <CommonRoute authRequire>
+              <Notification />
+            </CommonRoute>
+          }
+          path="/notifications"
+        />
+        <Route
+          exact
+          element={
+            <CommonRoute authRequire>
+              <ReportParent />
+            </CommonRoute>
+          }
+          path="/reports"
+        />
 
-        <Route exact element={<CommonRoute authRequire><Coupon /></CommonRoute>} path='/coupons' />
+        <Route
+          exact
+          element={
+            <CommonRoute authRequire>
+              <Coupon />
+            </CommonRoute>
+          }
+          path="/coupons"
+        />
 
-        <Route exact element={<CommonRoute authRequire><UserDetail /></CommonRoute>} path='/user/:userId' />
+        <Route
+          exact
+          element={
+            <CommonRoute authRequire>
+              <UserDetail />
+            </CommonRoute>
+          }
+          path="/user/:userId"
+        />
+        <Route path="*" element={<NavigateToCorrectPage />} />
       </Routes>
     </ThemeProvider>
-  )
-}
-export default App
+  );
+};
+export default App;
+
+const NavigateToCorrectPage = () => {
+  if (auth.currentUser) {
+    return <Navigate to="/" replace />;
+  } else {
+    return <Navigate to="/login" replace />;
+  }
+};
--- src/Components/UserDetail/index.js
+++ src/Components/UserDetail/index.js
@@ -1,28 +1,46 @@
-import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc, updateDoc, where } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { auth, db, functions } from "../../config/firebase-config";
 import { CircularProgress, Icon, IconButton } from "@mui/material";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { AWS_IMAGE_BASE_URL } from "../../config/appConfig";
 import dayjs from "dayjs";
 import { toast } from "react-toastify";
 import { deleteUser } from "firebase/auth";
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
-import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
 import { getFunctions, httpsCallable } from "firebase/functions";
 const UserDetail = () => {
-    const { userId } = useParams();
-    const [userData, setUserData] = useState(null);
-    const navigate = useNavigate();
-    const [confirmationText, setConfirmationText] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
-    const [loading,setLoading] = useState(false)
+  const { userId } = useParams();
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+  const [confirmationText, setConfirmationText] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     fetchUserData();
 
@@ -40,139 +58,171 @@ const UserDetail = () => {
   };
   const fetchUserData = async () => {
     try {
-      const userDocRef = doc(db, 'Users', userId); // Reference to the document with userId
+      const userDocRef = doc(db, "Users", userId); // Reference to the document with userId
       const userDocSnapshot = await getDoc(userDocRef); // Get the document snapshot
-      
+
       if (userDocSnapshot.exists()) {
         // Data exists for the provided user ID
         setUserData(userDocSnapshot.data());
       } else {
         // No data found for the provided user ID
-        console.log('No such document!');
+        console.log("No such document!");
       }
     } catch (error) {
-      console.error('Error getting document:', error);
+      console.error("Error getting document:", error);
     }
   };
   const handleClick = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     navigate(-1);
-  }
-    // const handleDelete = async(event) => {
-    //     event.preventDefault()
-    
-    // }
+  };
+  // const handleDelete = async(event) => {
+  //     event.preventDefault()
+
+  // }
 
   const handleConfirmationTextChange = (event) => {
     setConfirmationText(event.target.value);
   };
 
-  const handleDelete = async() => {
-    if (confirmationText === 'confirm') {
-    //   onDelete();
-    try {
-        setLoading(true)
-        const deleteUserAccount = httpsCallable(functions, 'deleteUserAccount');
+  const handleDelete = async () => {
+    if (confirmationText === "confirm") {
+      //   onDelete();
+      try {
+        setLoading(true);
+        const deleteUserAccount = httpsCallable(functions, "deleteUserAccount");
         await deleteUserAccount({
           userId,
-          username:userData.username
-        })
-        toast.error("User deleted!")
-        closeModal()
+          username: userData.username,
+        });
+        toast.error("User deleted!");
+        closeModal();
         navigate(-1);
-       } catch (error) {
-        toast.error("Something went wrong!")
-       }
-       finally{
-        setLoading(false)
-       }
+      } catch (error) {
+        toast.error("Something went wrong!");
+      } finally {
+        setLoading(false);
+      }
     }
   };
-  
+
   //blockUnblockUserAccount
   //userId
   //sendOnly userId
-    const handleBlock = async(event) => {
-        event.preventDefault()
-        try {
-            setLoading(true)
-            const toggleUserBlockStatus = httpsCallable(functions, 'toggleUserBlockStatus');
-            await toggleUserBlockStatus({
-              userId
-            })
-            if(userData.isBlocked)
-            toast.success("User unblocked!")
-            else
-            toast.error("User blocked!")
+  const handleBlock = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const toggleUserBlockStatus = httpsCallable(
+        functions,
+        "toggleUserBlockStatus"
+      );
+      await toggleUserBlockStatus({
+        userId,
+      });
+      if (userData.isBlocked) toast.success("User unblocked!");
+      else toast.error("User blocked!");
 
-            await fetchUserData()
-            setLoading(false)
-           } catch (error) {
-            console.log(error)
-            toast.error("Something went wrong!")
-           }
+      await fetchUserData();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
     }
-    const handleContactButtonClick = () => {
-        if (userData?.email) {
-          // If email is provided, open the default email app
-          window.open(`mailto:${userData?.email}`, '_blank');
-        } else if (userData?.phoneNumber) {
-          // If phone number is provided, open the default phone app
-          window.open(`tel:${userData?.phoneNumber}`, '_blank');
-        } else {
-          // Handle case when neither email nor phone number is provided
-          console.error('No email or phone provided for contact');
-        }
-      };
-    
-    return(
-        <div className="userprofilebody">
-            <div className="row">
-                <div className="col-xxl-5 col-xl-5 col-lg-7 col-md-8 profilecenter">
-                    <IconButton onClick={handleClick}>
-                        <ArrowBackIcon/>
-                    </IconButton>
-                    <div className="mainheading">
-                        <h3>User</h3>
-                    </div>
-             <div className="profileuserwrap">
-                <div className="userprofileimg">
-                    <img src="/assets/images/profilecover.png" alt=""/>
+  };
+  const handleContactButtonClick = () => {
+    if (userData?.email) {
+      // If email is provided, open the default email app
+      window.open(`mailto:${userData?.email}`, "_blank");
+    } else if (userData?.phoneNumber) {
+      // If phone number is provided, open the default phone app
+      window.open(`tel:${userData?.phoneNumber}`, "_blank");
+    } else {
+      // Handle case when neither email nor phone number is provided
+      console.error("No email or phone provided for contact");
+    }
+  };
+
+  return (
+    <div className="userprofilebody">
+      <div className="row">
+        <div className="col-xxl-5 col-xl-5 col-lg-7 col-md-8 profilecenter">
+          <IconButton onClick={handleClick}>
+            <ArrowBackIcon />
+          </IconButton>
+          <div className="mainheading">
+            <h3>User</h3>
+          </div>
+          <div className="profileuserwrap">
+            <div className="userprofileimg">
+              <img src="/assets/images/profilecover.png" alt="" />
+            </div>
+            <div className="profilebody">
+              <div className="userimg">
+                <img src={AWS_IMAGE_BASE_URL + userData?.profilePic} alt="" />
+                <span>
+                  <h2>{userData?.fullName}</h2>
+                  <div className="bangladeshprofile">
+                    <p>
+                      {userData?.phoneNumber
+                        ? userData?.phoneNumber
+                        : userData?.email}
+                    </p>
+                    <label>
+                      {" "}
+                      <img src="/assets/images/icons/location.svg" alt="" />
+                      {userData?.location}
+                    </label>
+                  </div>
+                </span>
+              </div>
+              <p>{userData?.biography}</p>
+              <h3>Information</h3>
+              <ul>
+                <div className="informationbody">
+                  <li>
+                    <span>
+                      <AccountCircleOutlinedIcon htmlColor="#A9A9A9" />
+                    </span>
+                    Username
+                  </li>
+                  <li>@{userData?.username}</li>
                 </div>
-                  <div className="profilebody">
-                      <div className="userimg">
-                        <img src={AWS_IMAGE_BASE_URL+userData?.profilePic} alt=""/>
-                           <span>
-                            <h2>{userData?.fullName}</h2>
-                            <div className="bangladeshprofile">
-                                <p>{userData?.phoneNumber ? userData?.phoneNumber : userData?.email}</p>
-                                <label> <img src="/assets/images/icons/location.svg" alt=""/>{userData?.location}</label>
-                            </div>
-                           </span>
-                      </div>
-                      <p>{userData?.biography}</p>
-                      <h3>Information</h3>
-                      <ul>
-                        <div className="informationbody">
-                            <li>
-                            <span>
-                                <AccountCircleOutlinedIcon htmlColor="#A9A9A9"/>
-                            </span>Username</li>
-                            <li>@{userData?.username}</li>
-                        </div>
-                        <div className="informationbody">
-                            <li><span> <EmailOutlinedIcon htmlColor="#A9A9A9"/></span>Email</li>
-                            <li>{userData?.email}</li>
-                        </div>
-                        <div className="informationbody">
-                            <li><span> <LocalPhoneOutlinedIcon htmlColor="#A9A9A9"/></span>Phone</li>
-                            <li>{userData?.phoneNumber}</li>
-                        </div>
-                        <div className="informationbody">
-                            <li><span> <CalendarMonthOutlinedIcon htmlColor="#A9A9A9"/></span>Joined</li>
-                            <li>{dayjs(userData ?.registredAt?.toDate())?.format('DD MMMM YYYY')}</li>
-                        </div>
-                        {/* <div className="informationbody">
+                <div className="informationbody">
+                  <li>
+                    <span>
+                      {" "}
+                      <EmailOutlinedIcon htmlColor="#A9A9A9" />
+                    </span>
+                    Email
+                  </li>
+                  <li>{userData?.email}</li>
+                </div>
+                <div className="informationbody">
+                  <li>
+                    <span>
+                      {" "}
+                      <LocalPhoneOutlinedIcon htmlColor="#A9A9A9" />
+                    </span>
+                    Phone
+                  </li>
+                  <li>{userData?.phoneNumber}</li>
+                </div>
+                <div className="informationbody">
+                  <li>
+                    <span>
+                      {" "}
+                      <CalendarMonthOutlinedIcon htmlColor="#A9A9A9" />
+                    </span>
+                    Joined
+                  </li>
+                  <li>
+                    {dayjs(userData?.registredAt?.toDate())?.format(
+                      "DD MMMM YYYY"
+                    )}
+                  </li>
+                </div>
+                {/* <div className="informationbody">
                             <li><span><img src="/assets/images/icons/language.svg" alt=""/></span>Language</li>
                             <li>English</li>
                         </div>
@@ -180,57 +230,68 @@ const UserDetail = () => {
                             <li><span><img src="/assets/images/icons/timezone.svg" alt=""/></span>Time zone</li>
                             <li>09/12/2024</li>
                         </div> */}
-                      </ul>
-                         <div className="profilebutton">
-                          <button onClick={handleContactButtonClick}><LocalPhoneOutlinedIcon htmlColor="#A9A9A9"/>
-                            
-                          Contact</button>
-                          <button disabled={loading} onClick={handleBlock}>{
-                            userData?.isBlocked?
-                            <CheckCircleOutlineOutlinedIcon htmlColor="green"/>:
-                            <CancelOutlinedIcon htmlColor="red"/>
-                          }{
-                            userData?.isBlocked?"Unblock":"Block"
-                          }
-                          {
-                              loading &&  <CircularProgress size={20} sx={{ color: "red" }} /> 
-                            }
-                          </button>
-                          <button className="active" onClick={openModal}><img src="/assets/images/icons/deleteprofile.svg" alt=""/>Delete</button>
-                         </div>
-                  </div>
-             </div>
-         </div>
+              </ul>
+              <div className="profilebutton">
+                <button onClick={handleContactButtonClick}>
+                  <LocalPhoneOutlinedIcon htmlColor="#A9A9A9" />
+                  Contact
+                </button>
+                <button disabled={loading} onClick={handleBlock}>
+                  {userData?.isBlocked ? (
+                    <CheckCircleOutlineOutlinedIcon htmlColor="green" />
+                  ) : (
+                    <CancelOutlinedIcon htmlColor="red" />
+                  )}
+                  {userData?.isBlocked ? "Unblock" : "Block"}
+                  {loading && (
+                    <CircularProgress size={20} sx={{ color: "red" }} />
+                  )}
+                </button>
+                <button className="active" onClick={openModal}>
+                  <img src="/assets/images/icons/deleteprofile.svg" alt="" />
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Dialog open={isOpen} onClose={closeModal}>
+        <DialogTitle>Delete Confirmation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To confirm deletion, please type <strong>"confirm"</strong> in the
+            input field below:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Confirmation"
+            fullWidth
+            value={confirmationText}
+            onChange={handleConfirmationTextChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeModal} color="primary" disabled={loading}>
+            Cancel
+          </Button>
+          <Button
+            endIcon={
+              loading ? (
+                <CircularProgress size={20} sx={{ color: "white" }} />
+              ) : null
+            }
+            onClick={handleDelete}
+            disabled={loading}
+            color="error"
+            variant="contained"
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
-    <Dialog open={isOpen} onClose={closeModal}>
-      <DialogTitle>Delete Confirmation</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          To confirm deletion, please type <strong>"confirm"</strong> in the input field below:
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Confirmation"
-          fullWidth
-          value={confirmationText}
-          onChange={handleConfirmationTextChange}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button
-          onClick={closeModal} color="primary" disabled={loading}>
-          Cancel
-        </Button>
-        <Button 
-            endIcon={loading ? <CircularProgress size={20} sx={{ color: "white" }} /> : null}
-            onClick={handleDelete} disabled={loading} color="error" variant="contained">
-          Delete
-        </Button>
-      </DialogActions>
-    </Dialog>
-
-    </div>
-    )
-}
-export default UserDetail
+  );
+};
+export default UserDetail;
